f901b7a933ef27b0e6fef12a7e1e94c923a45254
avacallery <avacallery@gmail.com>
2019-12-09 12:16:02 -0500

    input validation 04.11

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 Movie genre api
+
+//npm init
+//npm i express 
+    Express - a framework used for building web applications ontop of Node 
+//create index.js 
+//create express application by using express() function 
+    var express = require('express');
+    var app = express(); 
+//Specify your routes / route handlers using HTTP requests (req, res) 
+    //The req object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on. The object is always referred to as req (and the HTTP response is res) but its actual name is determined by the parameters to the callback function in which you're working. 
+//create Middleware
+    //Middleware functions are functions that have access to the request object (req), the response object (res), and the next function in the application’s request-response cycle. 
+    //The next function is a function in the Express router which, when invoked, executes the middleware succeeding the current middleware.
+//Use environment variable PORT to listen to the app using process.env.PORT
+    //const port = process.env.PORT || 3000; 
+    app.listen(port, () => { console.log('Listening on port ${port}')});
--- index.js
+++ index.js
@@ -1,32 +1,44 @@
-const express = require('express'); 
-const app = express(); 
+const express = require('express'); //returns a function 
+const app = express(); //call the function (which returns the object express()) and store it in a const app 
 
-app.use(express.json()); //idk what is going on here. parsing of data?
+//MIDDLEWARE
+app.use(express.json()); //in order for req.body.name to work, we must enable the parsing of a json object in the body of the request
+
 
 const movieGenres = [
     { id: 1, name: 'horror'}, 
     { id: 2, name: 'romance'}, 
     { id: 3, name: 'action'}, 
     { id: 4, name: 'sci-fi'}
-]
+];
 
+//HTTP methods get/put/post/delete 
+app.get('/', (req, res) => {
+    res.send('We are on the home of the movie api')
+});
 
+//get list of genres
 app.get('/api/genres', (req, res) => {
     res.send(movieGenres); 
 });
 
-app.get('/api/genres/:id', (req, res) => {
-    res.send(req.params.id); 
-});
-
+//create a new genre
 app.post('/api/genres', (req, res) => {
     
     const newMovie = {
         id: movieGenres.length + 1, 
-        name: req.body.name, //what is going on here? why body?
+        name: req.body.name, //we need to read this from the body of the request (assuming there is an object in the req body with a name property) 
         }; 
         movieGenres.push(newMovie); 
-        res.send(newMovie); 
+        res.send(newMovie); //when we post an object to the server, or when the server creates a new object, you return that object in the body of the response. Client should know the new id of this object 
+});
+
+//get a single genre using id parameter 
+app.get('/api/genres/:id', (req, res) => {
+    const findGenre = movieGenres.find(c => c.id === 
+        parseInt(req.params.id)); //this req will return a string, so we must parse into an integer
+    if (!findGenre) res.status(404).send('The genre with the given ID was not found in our collection.') //404 object not found
+    res.send(findGenre) //if genre is found, we use this method to return the genre back to the user 
 });
 
 //use environment variable to access port we use
